647142fb28cf781e143630a984616522806784de
daniapraezss <daniapraez@hotmail.com>
2021-07-13 23:20:12 -0500

    Dptos colombia

---
+++ departamento.js
@@ -0,0 +1,154 @@
+let letra = document.getElementById("letra");
+let letrados = document.getElementById("letrados");
+
+
+const traer = () => {
+
+    var departamento = document.getElementById("provincia").value;
+    console.log(departamento);
+
+    if (departamento == 1) {
+        
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Leticia_(Colombia)</a>";
+        letrados.innerHTML = "Leticia"
+
+    }
+
+    
+    if (departamento == 2) {
+      
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Medell%C3%ADn</a>";
+        letrados.innerHTML = "Medellín"
+    }
+    if (departamento == 3) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Arauca_(Colombia)</a>";
+        letrados.innerHTML = "Arauca"
+    }
+    if (departamento == 4) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Barranquilla</a>";
+        letrados.innerHTML = "Barranquilla"
+    }
+    if (departamento == 5) {
+        
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Cartagena_de_Indias</a>";
+        letrados.innerHTML = "Cartagena de Indias"
+    }
+    if (departamento == 6) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Tunja</a>";
+        letrados.innerHTML = "Tunja"
+    }
+    if (departamento == 7) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Manizales</a>";
+        letrados.innerHTML = "Manizales"
+    }
+    if (departamento == 8) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Florencia_(Caquet%C3%A1)</a>";
+        letrados.innerHTML = "Florencia"
+    }
+    if (departamento == 9) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Yopal</a>";
+        letrados.innerHTML = "Yopal"
+    }
+    if (departamento == 10) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Popay%C3%A1n</a>";
+        letrados.innerHTML = "Popayán"
+    }
+    if (departamento == 11) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Valledupar</a>";
+        letrados.innerHTML = "Valledupar"
+    }
+    if (departamento == 12) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Quibd%C3%B3</a>";
+        letrados.innerHTML = "Quibdó"
+    }
+    if (departamento == 13) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Monter%C3%ADa</a>";
+        letrados.innerHTML = "Montería"
+    }
+    if (departamento == 14) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Bogot%C3%A1</a>";
+        letrados.innerHTML = "Bogotá"
+    }
+    if (departamento == 15) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/In%C3%ADrida</a>";
+        letrados.innerHTML = "Inírida"
+    }
+    if (departamento == 16) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/San_Jos%C3%A9_del_Guaviare</a>";
+        letrados.innerHTML = "San José del Guaviare "
+    }
+    
+    if (departamento == 17) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Neiva</a>";
+        letrados.innerHTML = "Neiva"
+    
+    }
+    if (departamento == 18) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Riohacha</a>";
+        letrados.innerHTML = "Riohacha"
+    }
+    
+    if (departamento == 19) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Santa_Marta_(Colombia)</a>";
+        letrados.innerHTML = "Santa Marta"
+    }
+    
+    if (departamento == 20) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Villavicencio</a>";
+        letrados.innerHTML = "Villavicencio"
+    
+    }
+    if (departamento == 21) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Pasto_(Colombia)</a>";
+        letrados.innerHTML = "San Juan de Pasto"
+    }
+    if (departamento == 22) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Cucuta</a>";
+        letrados.innerHTML = "San José de Cúcuta"
+    }
+    if (departamento == 23) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Mocoa</a>";
+        letrados.innerHTML = "Mocoa"
+    }
+    if (departamento == 24) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Armenia</a>";
+        letrados.innerHTML = "Armenia"
+    }
+    if (departamento == 25) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Pereira</a>";
+        letrados.innerHTML = "Pereira"
+    }
+    if (departamento == 26) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/San_Andr%C3%A9s_(San_Andr%C3%A9s_y_Providencia)</a>";
+        letrados.innerHTML = "San Andrés"
+    }
+    if (departamento == 27) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Bucaramanga</a>";
+        letrados.innerHTML = "Bucaramanga"
+    }
+    if (departamento == 28) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Sincelejo</a>";
+        letrados.innerHTML = "Sincelejo"
+    }
+    if (departamento == 29) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Ibagu%C3%A9</a>";
+        letrados.innerHTML = "Ibagué"
+    }
+    if (departamento == 30) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Cali</a>";
+        letrados.innerHTML = "Cali"
+    }
+    if (departamento == 31) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Mit%C3%BA</a>";
+        letrados.innerHTML = "Mitú"
+    }
+    if (departamento == 32) {
+        letra.innerHTML = "<a href='tu cola' target='_blank'>https://es.wikipedia.org/wiki/Puerto_Carre%C3%B1o</a>";
+        letrados.innerHTML = "Puerto Carreño"
+    }
+    
+}
+
+const hola = () =>{
+    document.location("nombrepagina");
+}
